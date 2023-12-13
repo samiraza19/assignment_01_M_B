30b6d683a7ccb4a8847193e9e3ecafe96c5d740d
=SamiRaza <=samirazagb@gmail.com>
2023-12-13 08:51:09 +0500

    Updated Work

--- App.js
+++ App.js
@@ -1,113 +1,59 @@
-[
-  {
-  "id": 4,
-  "title": "newTitle",
-  "price": 205,
-  "description": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
-  "image_url": "https://i.imgur.com/R2PN9Wq.jpeg"
-  },
-  {
-  "id": 7,
-  "title": "Ergonomic Wooden Tuna",
-  "price": 743,
-  "description": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
-  "image_url": "https://i.imgur.com/mp3rUty.jpeg"
-  },
-  {
-  "id": 9,
-  "title": "Electronic Bronze Chips",
-  "price": 808,
-  "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
-  "image_url": "https://i.imgur.com/R3iobJA.jpeg"
-  },
-  {
-  "id": 11,
-  "title": "Awesome Bronze Car",
-  "price": 382,
-  "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
-  "image_url": "https://i.imgur.com/cBuLvBi.jpeg"
-  },
-  {
-  "id": 12,
-  "title": "Recycled Rubber Cheese",
-  "price": 30,
-  "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+   const arr =[
+    {
+    "id": 4,
+    "title": "newTitle",
+    "price": 205,
+    "description": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    "image_url": "https://i.imgur.com/R2PN9Wq.jpeg"
+    },
+    {
+    "id": 7,
+    "title": "Ergonomic Wooden Tuna",
+    "price": 743,
+    "description": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    "image_url": "https://i.imgur.com/mp3rUty.jpeg"
+    },
+    {
+    "id": 9,
+    "title": "Electronic Bronze Chips",
+    "price": 808,
+    "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+    "image_url": "https://i.imgur.com/R3iobJA.jpeg"
+    },
+    {
+    "id": 11,
+    "title": "Awesome Bronze Car",
+    "price": 382,
+    "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+    "image_url": "https://i.imgur.com/cBuLvBi.jpeg"
+    },
+    {
+    "id": 12,
+    "title": "Recycled Rubber Cheese",
+    "price": 30,
+    "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+    "image_url": "https://i.imgur.com/KeqG6r4.jpeg"
  }
  ]
 
-// No 1
 
-const user_list = document.getElementById("dataList1");
+ const cards = document.getElementById("cards")
+ for (let i = 0; i < arr.length ;i++){
 
-user_list.innerHTML = `<li>Name: ${userName}</li>
-<li>city: ${user_city}</li>
-<li>age</li>`;
+  const {id:card_id,
+    title:card_title,
+    price:card_price,
+    description:card_description,
+    image_url:card_img_url} = arr[i]
 
-const data_obj1 = {
-  title: "Product Title",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-  image_url:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
-};
+  cards.innerHTML += `<div class="card" style="width: 18rem;">
+  <img src="${card_img_url}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${card_title}</h5>
+    <p class="card-text">${card_description}</p>
+    ${card_price}
+    <a href="#" class="btn btn-primary">Order Now</a>
+  </div>
+</div>`
+ }
 
-// No 2
-const user_list = document.getElementById("dataList2");
-
-user_list.innerHTML = `<li>Name: ${userName}</li>
-<li>city: ${user_city}</li>
-<li>age</li>`;
-
-const data_obj2 = {
-  title: "Product Title",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-  image_url:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
-};
-
-
-// No 3
-const user_list = document.getElementById("dataList3");
-
-user_list.innerHTML = `<li>Name: ${userName}</li>
-<li>city: ${user_city}</li>
-<li>age</li>`;
-
-const data_obj3 = {
-  title: "Product Title",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-  image_url:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
-};
-
-// No 4
-const user_list = document.getElementById("dataList4");
-
-user_list.innerHTML = `<li>Name: ${userName}</li>
-<li>city: ${user_city}</li>
-<li>age</li>`;
-
-const data_obj4 = {
-  title: "Product Title",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-  image_url:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
-};
-
-// No 5
-const user_list = document.getElementById("dataList5");
-
-user_list.innerHTML = `<li>Name: ${userName}</li>
-<li>city: ${user_city}</li>
-<li>age</li>`;
-
-const data_obj5 = {
-  title: "Product Title",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-  image_url:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
-};
